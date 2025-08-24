4184c1ce85613a3c5e01d433c0dcdc05c7d973c8
John Byrne <jbyrnescu@yahoo.com>
2025-08-24 05:30:17 -0700

    cleaned up code. Removed unnecessary comments.

--- downloadList.js
+++ downloadList.js
@@ -31,15 +31,3 @@ for (let i = 0; i < nodes.length; i++) {
 		window.open(nodes[i].href, "_blank");
 	}
 }
-
-// for (let i = 0; i < nodes.snapshotLength; i++) {
-// 	let link = nodes.snapshotItem(i);
-// 	window.open(link.href, "_blank");
-// }
-
-// for (let node of nodes) {
-//	fetch(node).then(r => console.log(node, "status:", r.status));
-// }
-
-// nodes.forEach(node => window.open(node, "_blank"));
-
